eed1c930b3a55d41a21b65549753e0126bade592
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2022-08-08 15:33:01 -0500

    Ready

---
+++ src/assets/modules/bgnWeather.js
@@ -0,0 +1,44 @@
+const bgnWeather = [
+  {
+    title: "clearDay",
+    backgroundImage:
+      "linear-gradient(to right bottom, #246ac4, #028ad1, #39a7d6, #6fc1da, #a3dae0)",
+  },
+  {
+    title: "clearNight",
+    backgroundImage:
+      "linear-gradient(to right bottom, #0d1881, #091974, #081968, #0a185b, #0c174e)",
+  },
+  {
+    title: "cloudsDay",
+    backgroundImage:
+      "linear-gradient(to right bottom, #2676de, #2169c9, #1b5db4, #15519f, #0f458b)",
+  },
+  {
+    title: "cloudsNight",
+    backgroundImage:
+      "linear-gradient(to right bottom, #0e356f, #0e2f64, #0e2959, #0c244e, #0b1e44)",
+  },
+  {
+    title: "rainDay",
+    backgroundImage:
+      "linear-gradient(to right bottom, #2362b5, #1c6fb7, #277bb6, #3c87b4, #5391b1)",
+  },
+  {
+    title: "rainNight",
+    backgroundImage:
+      "linear-gradient(to right bottom, #183587, #003673, #00325a, #062d41, #1c252a)",
+  },
+  {
+    title: "thunderstormDay",
+    backgroundImage:
+      "linear-gradient(to right bottom, #1f4e97 #104581, #083c6a, #0a3254, #0e283f)",
+  },
+  {
+    title: "thunderstormNight",
+    backgroundImage:
+      "linear-gradient(to right bottom, #1a345e, #112c4e, #0b243e, #081c2e, #05131f)",
+  },
+];
+
+export default bgnWeather;
